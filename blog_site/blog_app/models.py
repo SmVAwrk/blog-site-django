5c8ca8e21f430df5b87b985c9998b5167d54096f
DS <doping5@list.ru>
2020-12-11 17:17:53 +0300

    Создание моделей и подключение БД (PostgreSQL)

--- blog_site/blog_app/models.py
+++ blog_site/blog_app/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    """Модель категорий"""
+    title = models.CharField(max_length=255, verbose_name='Название')
+    slug = models.SlugField(max_length=255, verbose_name='URL', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title', ]
+
+
+class Tags(models.Model):
+    """Модель тегов"""
+    title = models.CharField(max_length=50, verbose_name='Название')
+    slug = models.SlugField(max_length=255, verbose_name='URL', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+        ordering = ['title', ]
+
+
+class Posts(models.Model):
+    """Модель постов"""
+    title = models.CharField(max_length=255, verbose_name='Название')
+    slug = models.SlugField(max_length=255, verbose_name='URL', unique=True)
+    author = models.CharField(max_length=100, verbose_name='Автор')
+    content = models.TextField(blank=True, verbose_name='Контент поста')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, verbose_name='Фото')
+    views = models.IntegerField(default=0, verbose_name='Кол-во просмотров')
+    category = models.ForeignKey(Categories, on_delete=models.PROTECT, verbose_name='Категория', related_name='posts')
+    tags = models.ManyToManyField(Tags, blank=True, verbose_name='Теги', related_name='posts')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+        ordering = ['-created_at', ]
+
--- blog_site/blog_site/settings.py
+++ blog_site/blog_site/settings.py
@@ -77,9 +77,17 @@ WSGI_APPLICATION = 'blog_site.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blog_site_db',
+        'USER': 'dj_user',
+        'PASSWORD': '1q2w3e',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
