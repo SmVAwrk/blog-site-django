1ba3cf7f2f30c8cfebadfc29c0fd979e66cc3442
DS <doping5@list.ru>
2020-12-11 15:59:04 +0300

    Подключение статики и шаблонов

--- blog_site/blog_app/urls.py
+++ blog_site/blog_app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', hi, name='home'),
+    path('', index, name='home'),
 ]
--- blog_site/blog_app/views.py
+++ blog_site/blog_app/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def hi(request):
-    return HttpResponse('<h1>HI THERE!</h1>')
+def index(request):
+    return render(request, 'blog_app/index.html')
