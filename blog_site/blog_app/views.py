7f2a62ccba1634be30de09faa4a5f3e69562bb9f
DS <doping5@list.ru>
2020-12-13 22:28:42 +0300

    Добавление просмотра категории

--- blog_site/blog_app/views.py
+++ blog_site/blog_app/views.py
@@ -9,7 +9,7 @@ class Home(ListView):
     model = Posts
     template_name = 'blog_app/index.html'
     context_object_name = 'posts'
-    paginate_by = 1
+    paginate_by = 4
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -18,6 +18,23 @@ class Home(ListView):
 
     def get_queryset(self):
         return Posts.objects.filter(is_published=True, on_main=False).select_related('category', 'author')
+
+
+class PostsByCategory(ListView):
+    model = Posts
+    template_name = 'blog_app/category.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+    allow_empty = False
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Categories.objects.get(slug=self.kwargs['slug'])
+        return context
+
+    def get_queryset(self):
+        return Posts.objects.filter(is_published=True, category__slug=self.kwargs['slug']).select_related('category',
+                                                                                                          'author')
 
 
 class Post(DetailView):
