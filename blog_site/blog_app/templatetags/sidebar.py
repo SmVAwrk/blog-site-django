0af874bcb46d4fb0c2f7af8d615a3ed7184e328c
DS <doping5@list.ru>
2020-12-15 15:54:14 +0300

    Добавление вывода последних и популярных постов, облака тегов в сайдбаре

---
+++ blog_site/blog_app/templatetags/sidebar.py
@@ -0,0 +1,19 @@
+from django import template
+from django.db.models import Count, Sum
+
+from blog_app.models import Posts, Tags
+
+register = template.Library()
+
+
+@register.inclusion_tag('blog_app/sidebar_tpl.html')
+def get_sidebar_data(cnt=3):
+    recent = Posts.objects.filter(is_published=True).order_by('-created_at')[:cnt]
+    popular = Posts.objects.filter(is_published=True).order_by('-views')[:cnt]
+    tags = Tags.objects.annotate(sum=Sum('posts__views')).order_by('-sum')
+    return {'recent': recent, 'popular': popular, 'tags': tags}
+
+
+
+
+
